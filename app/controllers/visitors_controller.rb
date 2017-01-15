6e23a958ab76041798ca1e56d1d3d4077fafd1b5
Rhett Creighton <rhett@heyrhett.com>
2017-01-15 03:08:48 -0800

    update

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,15 +1,16 @@
 class VisitorsController < ApplicationController
   def index
     events = current_user.events
-    events.each do |event|
-      if !event.chainpoint
-        event.make_chainpoint
-      end
-      if event.chainpoint && !event.chainpoint.merkle_root
-        event.finish_chainpoint
+    if events
+      events.each do |event|
+        if !event.chainpoint
+          event.make_chainpoint
+        end
+        if event.chainpoint && !event.chainpoint.merkle_root
+          event.finish_chainpoint
+        end
       end
     end
-
   end
 
 end
